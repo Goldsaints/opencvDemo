38d0461bdc771710eea84227a60e3c3898c4b72d
ben <ben@google.com>
2020-12-02 19:52:38 +0800

    init

---
+++ opencvDemo/test.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/opencv.hpp>
+
+#include <iostream>
+
+using namespace std;
+
+using namespace cv;
+
+int main()
+
+{
+	//读取本地的一张图片便显示出来
+	Mat img = imread("9.jpg");
+	imshow("MM Viewer", img);
+
+	//等待用户按键
+	waitKey(0);
+	return 0;
+}
